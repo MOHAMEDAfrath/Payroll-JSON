adc65e8e1584a54344c5440361d9f139f4539d37
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-07 12:50:18 +0530

    [Afrath]Updated the file to add multiple employee to json server

--- PayrollJSON-Test/UnitTest1.cs
+++ PayrollJSON-Test/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Net;
 
@@ -29,7 +30,7 @@ namespace PayrollJSON_Test
         {
             IRestResponse response = GetAllEmployee();
             List<Employee> list = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            Assert.AreEqual(7,list.Count);
+            Assert.AreEqual(6,list.Count);
             Assert.AreEqual(HttpStatusCode.OK,response.StatusCode);
             foreach(var mem in list)
                 System.Console.WriteLine(mem.id+" "+mem.first_name+" "+mem.last_name+" "+mem.email);
@@ -49,7 +50,38 @@ namespace PayrollJSON_Test
             var result = JsonConvert.DeserializeObject<Employee>(response.Content);
             Assert.AreEqual("Ram", result.first_name);
             Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
-
+        }
+        //add data to json server
+        public void add(JsonObject jsonObject)
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+             request.AddParameter("application/json", jsonObject, ParameterType.RequestBody);
+             IRestResponse response = restClient.Execute(request);
+            
+        }
+        [TestMethod]
+        public void TestMethod_ToTest_MultipleInsertion()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            List<JsonObject> list = new List<JsonObject>();
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.Add("first_name", "Priya");
+            jsonObject.Add("last_name", "Devi");
+            jsonObject.Add("email", "priyadevi@gmail.com");
+            list.Add(jsonObject);
+            JsonObject jsonObject1 = new JsonObject();
+            jsonObject1.Add("first_name", "Dhanush");
+            jsonObject1.Add("last_name", "Raj");
+            jsonObject1.Add("email", "DhanushRaj@gmail.com");
+            list.Add(jsonObject1);
+            foreach (var mem in list)
+                add(mem);
+            //retrieve to check the count after insertion
+            IRestResponse response = GetAllEmployee();
+            List<Employee> result = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(9, result.Count);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+               
         }
     }
 }
