8bfe9b12726857c6e35e60f851dffb061142c307
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-08 12:49:13 +0530

    [Afrath]Updated the file to add employee to json server

--- PayrollJSON-Test/UnitTest1.cs
+++ PayrollJSON-Test/UnitTest1.cs
@@ -29,10 +29,10 @@ namespace PayrollJSON_Test
         {
             IRestResponse response = GetAllEmployee();
             List<Employee> list = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            Assert.AreEqual(7,list.Count);
+            Assert.AreEqual(6,list.Count);
             Assert.AreEqual(HttpStatusCode.OK,response.StatusCode);
             foreach(var mem in list)
-                System.Console.WriteLine(mem.id+" "+mem.first_name+" "+mem.last_name+" "+mem.email);
+                System.Console.WriteLine(mem.id+" "+mem.first_name+" "+mem.last_name+" "+mem.salary);
 
         }
         //test the insertion
@@ -43,7 +43,7 @@ namespace PayrollJSON_Test
             JsonObject jsonObject = new JsonObject();
             jsonObject.Add("first_name", "Ram");
             jsonObject.Add("last_name", "Kumar");
-            jsonObject.Add("email", "ramkumar@gmail.com");
+            jsonObject.Add("salary", 25000);
             request.AddParameter("application/json", jsonObject, ParameterType.RequestBody);
             IRestResponse response = restClient.Execute(request);
             var result = JsonConvert.DeserializeObject<Employee>(response.Content);
